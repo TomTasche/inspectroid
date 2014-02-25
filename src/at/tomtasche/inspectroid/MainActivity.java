e77f6b85bb98d3790f9e8ddc97df805545265332
Thomas Taschauer <tomtasche@gmail.com>
2014-02-25 11:26:05 +0100

    improve texts

--- src/at/tomtasche/inspectroid/MainActivity.java
+++ src/at/tomtasche/inspectroid/MainActivity.java
@@ -109,8 +109,8 @@ public class MainActivity extends ListActivity implements
 			title = "Blocking HTTP";
 			titleCondensed = "Blocking";
 		} else {
-			title = "Not blocking HTTP";
-			titleCondensed = "Not blocking";
+			title = "Only logging HTTP";
+			titleCondensed = "Only logging";
 		}
 
 		httpItem.setTitle(title);
